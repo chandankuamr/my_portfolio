2730240eb1d569a6bcc8bf4e63a1515cfcb3c03c
chandan <c.kumar.in07@gmail.com>
2026-09-17 15:50:36 +0530

    Initial portfolio website

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request
+import mysql.connector
+from dotenv import load_dotenv
+import os
+app = Flask(__name__)
+load_dotenv()
+
+db = mysql.connector.connect(
+    host=os.getenv("MYSQL_HOST"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    database=os.getenv("MYSQL_DATABASE")
+)
+print("MySQL connected successfully!")
+
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    name = request.form["name"]
+    email = request.form["email"]
+    message = request.form["message"]
+
+    print("Name:", name)
+    print("Email:", email)
+    print("Message:", message)
+
+    cursor = db.cursor()
+
+    query = """
+    INSERT INTO messages (name, email, message)
+    VALUES (%s, %s, %s)
+    """
+
+    cursor.execute(query, (name, email, message))
+    db.commit()
+
+    cursor.close()
+
+    return "Message saved successfully!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
