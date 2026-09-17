4808790a34ec855263d8511b97e2541ae93811e0
chandan <c.kumar.in07@gmail.com>
2026-09-17 17:11:27 +0530

    Fix MySQL port

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ db = mysql.connector.connect(
     host=os.getenv("MYSQL_HOST"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
-    database=os.getenv("MYSQL_DATABASE")
+    database=os.getenv("MYSQL_DATABASE"),
+    port=int(os.getenv("MYSQL_PORT"))
 )
 print("MySQL connected successfully!")
 
